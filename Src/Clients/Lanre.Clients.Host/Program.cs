8f93095929be2adf9d68d9bf1f5420f52c474b70
= <etorre@plainconcepts.com>
2019-08-16 15:57:10 +0200

    Add serilog

--- Src/Clients/Lanre.Clients.Host/Program.cs
+++ Src/Clients/Lanre.Clients.Host/Program.cs
@@ -5,16 +5,47 @@ namespace Lanre.Clients.Host
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Events;
+    using Serilog.Sinks.SystemConsole.Themes;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                // Close and flush the log.
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateHostBuilder(string[] args) =>
              WebHost.CreateDefaultBuilder(args)
+                .UseSerilog((builderContext, config) => CreateSerilogLogger(builderContext, config))
                 .UseStartup<Startup>();
+
+        private static void CreateSerilogLogger(WebHostBuilderContext builderContext, LoggerConfiguration config)
+        {
+            config
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", "Lanre")
+                .Enrich.FromLogContext()
+                .Enrich.WithCorrelationId()
+                .WriteTo.Console(
+                    LogEventLevel.Information,
+                    "{NewLine}[{Timestamp:HH:mm:ss} {Level}-{ConnectionId}] {Message}{NewLine}{Exception}",
+                    theme: AnsiConsoleTheme.Code)
+                .WriteTo.AzureApp(
+                    LogEventLevel.Warning,
+                    "{NewLine}[{Timestamp:HH:mm:ss} {Level}-{ConnectionId}] {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            Log.Logger = config as Serilog.ILogger;
+        }
     }
 }
